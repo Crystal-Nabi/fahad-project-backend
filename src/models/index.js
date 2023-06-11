c8ae7b911909b971ece26ca1ad600db7f88b52fa
paullong0505 <paullong0505@gmail.com>
2023-06-12 03:24:33 +0900

    feat

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,11 @@
 const dbConfig = require("../config/db.config.js");
-
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 mongoose.Promise = global.Promise;
-
 const db = {};
+console.log(process.env.DB_URL);
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = process.env.DB_URL;
 db.catagories = require("./catagoryModel.js")(mongoose);
 
 module.exports = db;
