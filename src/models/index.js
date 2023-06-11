2d2189c9c09bec67d897285111d107ff25563b5c
paullong0505 <paullong0505@gmail.com>
2023-06-12 06:30:28 +0900

    init

--- src/models/index.js
+++ src/models/index.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url =
-  "mongodb+srv://paul:IlVG4JbRJ5ItBjrj@cluster0.kw5dyri.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://IlVG4JbRJ5ItBjrj:IlVG4JbRJ5ItBjrj@cluster0.kw5dyri.mongodb.net/?retryWrites=true&w=majority";
 db.catagories = require("./catagoryModel.js")(mongoose);
 
 module.exports = db;
